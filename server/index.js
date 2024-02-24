2ae028c624b2da344dd79c6b426a61ddc55de196
Quarkyy77 <tushcosmo@gmail.com>
2024-02-24 11:44:09 +0530

    updated backend config

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(
   cors({
-    origin: ["https://deploy-project1-frontend.vercel.app/"],
+    origin: ["https://deploy-project1-frontend.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true,
   })
